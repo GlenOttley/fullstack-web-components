89514419715c528ac90c398957b5a52e858d4232
Glen Ottley <glenottley@gmail.com>
2023-03-07 07:42:14 -0500

    lesson 1

---
+++ packages/client/src/view/main/MainView.ts
@@ -0,0 +1,31 @@
+import { Component, attachShadow, html, css } from '@in/common';
+
+const styles = css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+  }
+  #content-root {
+    overflow-y: scroll;
+  }
+`;
+
+const shadowTemplate = html`
+  <header>Header</header>
+  <div id="content-root">Content</div>
+  <footer>Footer</footer>
+`;
+
+@Component({
+  selector: 'main-view',
+  style: styles,
+  template: shadowTemplate,
+})
+export class MainView extends HTMLElement {
+  constructor() {
+    super();
+    attachShadow(this);
+  }
+}
